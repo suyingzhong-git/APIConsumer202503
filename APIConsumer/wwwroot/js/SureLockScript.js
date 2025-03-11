fbb7323e0ae8f70b48d58824425782aa057db9df
szhong <zhongsuying@gmail.com>
2025-03-11 11:49:11 +0000

    Added "Update Use JavaScript" on Edit page

--- APIConsumer/wwwroot/js/SureLockScript.js
+++ APIConsumer/wwwroot/js/SureLockScript.js
@@ -29,3 +29,33 @@ async function JSDeleteUseAPI() {
         .catch(error => console.error("Error:", error));
     document.location.href = "/SureLockTest/index";
 }
+
+async function JSEdit() {
+    //alert(idVals[0].toString());
+    const lockObj =new Object();
+    lockObj.id=document.getElementById("id").value;
+    lockObj.name = document.getElementById("name").value;
+    lockObj.price = document.getElementById("price").value;
+    lockObj.quantity = document.getElementById("quantity").value;
+    const lockData=JSON.stringify(lockObj);
+    //alert(lockData);
+    console.log(lockData);
+    await fetch(
+        "https://surelock.vercel.app/api/products",
+        {
+            method: "PATCH",
+            headers: {
+                "Authorization": "Bearer a642ed09-deb5-4b5c-84ec-99e61a39e20c",
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: lockData
+        }
+    )
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.error("Error:", error));
+    document.location.href = "/SureLockTest/index";
+}
+
+
